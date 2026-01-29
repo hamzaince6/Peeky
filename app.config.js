8bfb7ce7996ce3eb897a586fdccfb20f7f5a643e
Hamza ince <hamzaince001@gmail.com>
2026-01-29 14:30:21 +0300

    Increment buildNumber and versionCode to 4 in app.config.js and app.json for iOS and Android.

--- app.config.js
+++ app.config.js
@@ -20,7 +20,7 @@ module.exports = {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.peeky.educationalgames',
-      buildNumber: '3',
+      buildNumber: '4',
       icon: './assets/peekylogo.png',
       infoPlist: {
         NSLocalNetworkUsageDescription: 'This app needs access to your local network',
@@ -35,7 +35,7 @@ module.exports = {
       edgeToEdgeEnabled: true,
       predictiveBackGestureEnabled: false,
       package: 'com.peeky.educationalgames',
-      versionCode: '3',
+      versionCode: '4',
       permissions: [
         'android.permission.INTERNET',
         'android.permission.CAMERA',
